fbf79b4fab47cc1ff6bb6cc8c16a8ff0be25b95e
MohammadAmin Vahedinia <mohammadamin.vahedinia@cafebazaar.ir>
2021-07-24 23:18:48 +0430

    Clean runner

--- run.py
+++ run.py
@@ -1,18 +1,14 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
 from miyanmaayeh.runner import Runner
 
 
 def main():
     config = {
-        "snapshots_in": [0, 1, 2, 3, 4, 5, 25, 50],
-        # "snapshots_in": [0, 24, 49, 74, 99],
+        "snapshots_in": [i for i in range(0, 200, 25)],
         "plot_dir": "./plots/",
         "fundamentalist_count": 50,
     }
 
-    run_time = 100
+    run_time = 200
 
     runner = Runner(config=config)
     runner.run(run_time)
