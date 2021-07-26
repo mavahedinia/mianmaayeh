625ded76617c9190547960afbafd3d524bc1b7f8
MohammadAmin Vahedinia <mohammadamin.vahedinia@cafebazaar.ir>
2021-07-26 15:24:45 +0430

    Add ContrarianAgent and TechnicalAnalyst agent

--- miyanmaayeh/agent.py
+++ miyanmaayeh/agent.py
@@ -27,7 +27,7 @@ class Agent:
         market_prices = np.array(market_price_history)
         return (noise * market_prices).tolist()
 
-    def analyze(self, market_history) -> MarketAction:
+    def analyze(self, market_history: MarketHistory) -> MarketAction:
         return MarketAction(action_type=ActionType.Skip.value, amount=0, bid=0, agent=self)
 
     def get_action(self, market_history) -> MarketAction:
@@ -72,14 +72,135 @@ class Agent:
 
 
 class FundamentalistAgent(Agent):
-    def analyze(self, market_history):
+    def analyze(self, market_history: MarketHistory):
         market_indicator = np.random.rand(1) * 3
         bid = np.random.uniform(50, 100)
         if len(market_history) > 0:
             bid = market_history[-1].price_equilibrium
 
         if market_indicator > 2:
-            return MarketAction(action_type=ActionType.Buy.value, agent=self, amount=0, bid=bid)
+            return MarketAction(
+                action_type=ActionType.Buy.value,
+                agent=self,
+                amount=0,
+                bid=bid,
+            )
         elif market_indicator > 1:
-            return MarketAction(action_type=ActionType.Skip.value, agent=self, amount=0, bid=0)
-        return MarketAction(action_type=ActionType.Sell.value, agent=self, amount=0, bid=bid)
+            return MarketAction(
+                action_type=ActionType.Skip.value,
+                agent=self,
+                amount=0,
+                bid=0,
+            )
+        return MarketAction(
+            action_type=ActionType.Sell.value,
+            agent=self,
+            amount=0,
+            bid=bid,
+        )
+
+
+class ContrarianAgent(Agent):
+    EPS = 0.05
+
+    def analyze(self, market_history: MarketHistory) -> MarketAction:
+        if len(market_history) > 0:
+            total_actions = market_history[-1].buy_action_count + market_history[-1].sell_action_count
+            market_indicator = (market_history[-1].buy_action_count - market_history[-1].sell_action_count) / total_actions
+            bid = market_history[-1].price_equilibrium
+        else:
+            market_indicator = np.random.uniform(-2, 2)
+            bid = np.random.uniform(50, 100)
+
+        if market_indicator > self.EPS:
+            return MarketAction(
+                ActionType.Sell.value,
+                agent=self,
+                amount=0,
+                bid=bid,
+            )
+        elif market_indicator < -self.EPS:
+            return MarketAction(
+                ActionType.Buy.value,
+                agent=self,
+                amount=0,
+                bid=bid,
+            )
+        return MarketAction(
+            ActionType.Skip.value,
+            agent=self,
+            amount=0,
+            bid=0,
+        )
+
+
+class TechnicalAnalystAgent(Agent):
+    EPS = 0.05
+
+    @staticmethod
+    def ewma_vectorized(data, alpha, offset=None, dtype=None, order="C", out=None):
+        data = np.array(data, copy=False)
+        if dtype is None:
+            if data.dtype == np.float32:
+                dtype = np.float32
+            else:
+                dtype = np.float64
+        else:
+            dtype = np.dtype(dtype)
+
+        if data.ndim > 1:
+            data = data.reshape(-1, order)
+        if out is None:
+            out = np.empty_like(data, dtype=dtype)
+        else:
+            assert out.shape == data.shape
+            assert out.dtype == dtype
+        if data.size < 1:
+            return out
+
+        if offset is None:
+            offset = data[0]
+
+        alpha = np.array(alpha, copy=False).astype(dtype, copy=False)
+        scaling_factors = np.power(1.0 - alpha, np.arange(data.size + 1, dtype=dtype), dtype=dtype)
+        np.multiply(data, (alpha * scaling_factors[-2]) / scaling_factors[:-1], dtype=dtype, out=out)
+        np.cumsum(out, dtype=dtype, out=out)
+        out /= scaling_factors[-2::-1]
+        if offset != 0:
+            offset = np.array(offset, copy=False).astype(dtype, copy=False)
+            out += offset * scaling_factors[1:]
+        return out
+
+    def analyze(self, market_history: MarketHistory) -> MarketAction:
+
+        if len(market_history) > 0:
+            prices_list = [item.price_equilibrium for item in market_history]
+            ewma_12_days = self.ewma_vectorized(prices_list, 2 / (12 + 1))
+            ewma_26_days = self.ewma_vectorized(prices_list, 2 / (26 + 1))
+            MACD_ind = (ewma_12_days - ewma_26_days)[-1]
+
+            bid = market_history[-1].price_equilibrium
+        else:
+            MACD_ind = np.random.uniform(-1, 1)
+            bid = np.random.uniform(50, 100)
+
+        if MACD_ind > self.EPS:
+            return MarketAction(
+                ActionType.Buy.value,
+                agent=self,
+                amount=0,
+                bid=bid,
+            )
+        elif MACD_ind < -self.EPS:
+            return MarketAction(
+                ActionType.Sell.value,
+                agent=self,
+                amount=0,
+                bid=bid,
+            )
+        return MarketAction(
+            ActionType.Skip.value,
+            agent=self,
+            amount=0,
+            bid=0,
+        )
--- run.py
+++ run.py
@@ -1,14 +1,19 @@
+import math
+
 from miyanmaayeh.runner import Runner
 
 
 def main():
-    config = {
-        "snapshots_in": [i for i in range(0, 200, 25)],
-        "plot_dir": "./plots/",
-        "fundamentalist_count": 50,
-    }
+    run_time = 500
+    steps = math.ceil(run_time / 5)
 
-    run_time = 200
+    config = {
+        "snapshots_in": [i for i in range(0, run_time, steps)] + [run_time - 1],
+        "plot_dir": "./plots/",
+        "fundamentalist_count": 20,
+        "contrarian_count": 20,
+        "technical_analyst_count": 20,
+    }
 
     runner = Runner(config=config)
     runner.run(run_time)
